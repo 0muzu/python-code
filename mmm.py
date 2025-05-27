0d4b7c72240979434bd4fd6878edd146ee1cb825
Müzəffər <muzuqafarli@gmail.com>
2025-05-27 22:47:58 +0400

    Update mmm.py

--- mmm.py
+++ mmm.py
@@ -2,14 +2,12 @@ il = int(input("Doğum ilini yaz: "))
 ay = int(input("Doğum ayını yaz (1-12): "))
 gun = int(input("Doğum gününü yaz: "))
 
-# Bugünkü tarixi özümüz təyin edirik (sabit şəkildə)
 bugun_il = 2025
 bugun_ay = 5
 bugun_gun = 27
 
 yas = bugun_il - il
 
-# Əgər hələ ad günü gəlməyibsə, yaşı 1 azaldırıq
 if (ay > bugun_ay) or (ay == bugun_ay and gun > bugun_gun):
     yas -= 1
 
